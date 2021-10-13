d9f8e2b7abec27ef07feb32dc9c3672e93d4533c
MainAdd <jokairytb@gmail.com>
2021-10-13 15:06:52 +0200

    retrait de BellatorMMA

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -16,23 +16,23 @@ export default function home() {
                     </li>
                     <li>
                         <img src="https://www.fabutainment.com/wp-content/uploads/2021/10/C07_0162.jpg" alt="Error" />
-                        <h2><a href="https://dood.ws/d/lucgu7o398b1" target="_blank">[BellatorMMA] LUKE TRAINER VS YANNICK BAHATI - FULL FIGHT</a></h2>
+                        <h2><a href="https://dood.ws/d/lucgu7o398b1" target="_blank">LUKE TRAINER VS YANNICK BAHATI - FULL FIGHT</a></h2>
                     </li>
                     <li>
                         <img src="https://mmajunkie.usatoday.com/wp-content/uploads/sites/91/2021/10/Tim-Wilde-vs-Yves-Landu-Bellator-267-2.jpg?w=1000" alt="Error" />
-                        <h2><a href="https://dood.ws/d/ffypx6o1kkdd" target="_blank">[BellatorMMA] YVES LANDU VS TIM WILDE - FULL FIGHT</a></h2>
+                        <h2><a href="https://dood.ws/d/ffypx6o1kkdd" target="_blank">YVES LANDU VS TIM WILDE - FULL FIGHT</a></h2>
                     </li>
                     <li>
                         <img src="https://cdn.vox-cdn.com/thumbor/J_WiotSXDbaaSvsk3D-t3OJMv90=/0x0:3850x2567/1200x800/filters:focal(1674x530:2290x1146)/cdn.vox-cdn.com/uploads/chorus_image/image/69954962/C07_0164.0.jpg" alt="Error" />
-                        <h2><a href="https://dood.ws/d/6oaoetckoqls" target="_blank">[BellatorMMA] MICHAEL PAGE VS DOUGLAS LIMA - FULL FIGHT</a></h2>
+                        <h2><a href="https://dood.ws/d/6oaoetckoqls" target="_blank">MICHAEL PAGE VS DOUGLAS LIMA - FULL FIGHT</a></h2>
                     </li>
                     <li>
                         <img src="https://mmajunkie.usatoday.com/wp-content/uploads/sites/91/2021/10/Leah-McCourt-vs-Jessica-Borga-Bellator-267-3.jpg?w=1000" alt="Error" />
-                        <h2><a href="https://dood.ws/d/sqsgiwghpx95" target="_blank">[BellatorMMA] LEAH McCOURT VS JESSICA BORGA - FULL FIGHT</a></h2>
+                        <h2><a href="https://dood.ws/d/sqsgiwghpx95" target="_blank">LEAH McCOURT VS JESSICA BORGA - FULL FIGHT</a></h2>
                     </li>
                     <li>
                         <img src="https://mmajunkie.usatoday.com/wp-content/uploads/sites/91/2021/10/Robert-Whiteford-vs-Andrew-Fisher-Bellator-267.jpg?w=1000" alt="Error" />
-                        <h2><a href="https://dood.ws/d/ksbyhmg78f1k" target="_blank">[BellatorMMA] ROBERT WHITEFORD VS ANDREW FISHER - FULL FIGHT</a></h2>
+                        <h2><a href="https://dood.ws/d/ksbyhmg78f1k" target="_blank">ROBERT WHITEFORD VS ANDREW FISHER - FULL FIGHT</a></h2>
                     </li>
                     <li>
                         <img src="https://images.daznservices.com/di/library/DAZN_News/4e/50/chris-colbert_hveyumm8f0gi13mub5oec431e.jpg?t=1899494298&quality=60&h=450" alt="Error" />
